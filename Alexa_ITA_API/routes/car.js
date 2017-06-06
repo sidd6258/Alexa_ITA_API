0bfd577ce6b4ef5cde55c54e46fd476b51c413f7
siddharthgupta <siddharth6258@gmail.com>
2017-06-05 19:13:28 -0700

    ITA-35 bugfixes

--- Alexa_ITA_API/routes/car.js
+++ Alexa_ITA_API/routes/car.js
@@ -16,36 +16,48 @@ exports.search= function(req,resp) {
 	var cars=[];
 	var speechText = "";
 	var option = 0;
-	
+	console.log("car object is "+carObject);
+
 	request({
 	    url: "https://homerest.herokuapp.com/req/car",
 	    method: "POST",
 	    json: true,   // <--Very important!!!
 	    body: carObject
 	}, function (error, response, body){
-		
+		console.log("car object is "+carObject);
 		if (!error && response.statusCode == 200) {
-			for(i=0;i<2;i++){
-			details = response.body.carinfo[i];
-			option = i+1;
-			if(option == 1)
+			if(response.body.carinfo.length > 0)
 				{
-				speechText += "the top 2 search results are. Option "+option+", "+ details["model"]+" with features "+details["features"];
-				speechText += " and seating avaialble for "+details["typicalseating"] + " Total price is "+ details["totalprice"]+" " +details["currencycode"]+". ";
+				for(i=0;i<2;i++){
+					details = response.body.carinfo[i];
+					option = i+1;
+					if(option == 1)
+						{
+						speechText += "the top 2 search results are. Option "+option+", "+ details["model"]+" with features "+details["features"];
+						speechText += " and seating avaialble for "+details["typicalseating"] + " Total price is "+ details["totalprice"]+" " +details["currencycode"]+". ";
+						}
+					else{
+						speechText += " Option "+option+", "+ details["model"]+" with features "+details["features"];
+						speechText += " and seating avaialble for "+details["typicalseating"] + " Total price is "+ details["totalprice"]+" " +details["currencycode"]+". ";
+					}
+					}
+					var respon={"statusCode":200,
+		    				"cars":speechText
+		    			};
+					console.log("Response from the request of Rental car search is "+respon);
+					resp.send(respon);
 				}
 			else{
-				speechText += " Option "+option+", "+ details["model"]+" with features "+details["features"];
-				speechText += " and seating avaialble for "+details["typicalseating"] + " Total price is "+ details["totalprice"]+" " +details["currencycode"]+". ";
+				speechText += "no results found";
+				var respon={"statusCode":200,
+	    				"cars":speechText
+	    			};
+				resp.send(respon);
 			}
-			}
-			var respon={"statusCode":200,
-    				"cars":speechText
-    			};
-			console.log("Response from the request of Rental car search is "+respon);
-			resp.send(respon);
+
 		}else{
 			console.log(response.status=500);
-			res.send(response);
+			resp.send(response);
 		}
 	});
 	
