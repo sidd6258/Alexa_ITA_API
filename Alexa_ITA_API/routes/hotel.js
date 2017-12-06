fe4cf1b257075b9a8c90b33eaf4ded464632eed3
siddharthgupta <siddharth6258@gmail.com>
2017-12-06 12:22:53 -0800

    ITA-65 test

--- Alexa_ITA_API/routes/hotel.js
+++ Alexa_ITA_API/routes/hotel.js
@@ -7,7 +7,9 @@ var mongo = require("../routes/mongo");
 var mongoURL = "mongodb://ainuco.ddns.net:4325/iTravelDB";
 var mysql = require("./mysql");
 var config = require('./config');
-var client = require('./connection.js');  
+var client = require('./connection.js'); 
+var nodemailer = require("nodemailer");
+
 
 Date.prototype.addDays = function(days) {
     var dat = new Date(this.valueOf())
@@ -162,6 +164,16 @@ function getTop3Raters(hotels,callback){
 		    }
 		    else {
 		        console.log("Successfully inserted details in MYSQL");
+		        //MONGO CALL
+		        mailobj={
+		        		"email": email,
+		        		"booking": module,
+		        		"hotelname": "Mongo call",
+		        		"startdate": start_date,
+		        		"enddate":end_date,
+		        		"amount":price
+		        }
+		        sendmail(mailobj);
 		    	var respon={"statusCode":200};
 		    	resp.send(respon);
 		    }
@@ -373,3 +385,19 @@ exports.elasticsearch=function(req,res){
     
 		
 	}
+
+function sendmail(obj){
+    var mailOptions={
+            to : obj['email'],
+            subject : "Congratulations for your Hotel Booking",
+            text : "Hi, you have booked "+obj["hotelname"]+ " from "+obj["startdate"]+" to "+obj["enddate"]+" for "+obj["price"]
+        }
+        console.log(mailOptions);
+        smtpTransport.sendMail(mailOptions, function(error, response){
+         if(error){
+                console.log(error);
+         }else{
+                console.log("Message sent: " + response);
+             }
+    });
+ };
